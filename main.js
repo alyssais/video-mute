1acf1e7c6338c74871dc084f8bfbd0e74c6fef6c
Ross Penman <ross@pen.mn>
2014-12-29 20:53:18 +0000

    Better downloading, no prompting

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
 var input = document.getElementById("video-input");
 var ffmpeg = document.getElementById("ffmpeg-script");
 var message = document.getElementById("loading-message");
+var link = document.getElementById("download-link");
 var ffmpegLoaded = false;
+
+// ffmpeg keeps calling this but from what I can tell the result doesn't matter.
+window.prompt = function() {
+  return "";
+};
 
 var processFiles = function() {
   if (!ffmpegLoaded || input.files.length === 0) return;
-  message.innerHTML = "converting video";
+  link.hidden = true;
+  message.innerHTML = "removing audio";
   
   var file = input.files[0];
-  console.log(file);
   var reader = new FileReader();
   
   reader.onload = function(e) {
@@ -24,13 +30,11 @@ var processFiles = function() {
       ],
       files: [{data: data, name: file.name}]
     });
-    var blob = new Blob([result[0].data], {type: file.type});
-    message.innerHTML = "finished converting";
-    var a = document.createElement("a");
-    a.innerHTML = "download";
-    a.href = URL.createObjectURL(blob);
-    a.download = file.name;
-    document.body.appendChild(a);
+    var blob = new Blob([result[0].data], {type: "video/mp4"});
+    message.innerHTML = "finished";
+    link.hidden = false;
+    link.href = URL.createObjectURL(blob);
+    link.download = file.name;
   };
   
   reader.readAsArrayBuffer(file);
@@ -49,3 +53,4 @@ ffmpeg.onerror = function(e) {
 input.addEventListener("change", processFiles);
 
 input.hidden = false;
+message.hidden = false;
